da289acf13c98651dbd0c45d400f553f06d8b035
mdmirajhossansajid <mdmirajhossansajidpc@gmail.com>
2026-03-20 16:08:05 +0600

    optimize cashout part

--- script/home.js
+++ script/home.js
@@ -1,32 +1,27 @@
 document.getElementById("cashout-btn").addEventListener("click",function(){
-    const  cashOutNumberInput=document.getElementById("cashout-number");
-    const cashOutNumber=cashOutNumberInput.value;
+    
+   const  cashOutNumber=getValueFromInput("cashout-number")
     if(cashOutNumber.length!=11){
         alert("Invalid Agent Number");
         return;
     }
 
-    const  cashOutAmountInput=document.getElementById("cashout-amount");
-    const cashOutAmount=cashOutAmountInput.value;
-    console.log(cashOutAmount);
+    const cashOutAmount=getValueFromInput("cashout-amount")
 
-    const currentBalanceElement=document.getElementById("balance");
-    const currentBalance=currentBalanceElement.innerText;
-    
-    const newBalance=Number(currentBalance)-Number(cashOutAmount);
+    const currentBalance=getBalance();
+    const newBalance=currentBalance-Number(cashOutAmount);
     if(newBalance<0)
        {
          alert("Invalid Amount");
          return;
        }
 
-    const cashOutPinInput=document.getElementById("cashout-pin");
-    const pin=cashOutPinInput.value;
+    const pin=getValueFromInput("cashout-pin")
     if(pin==="1234")
        {
          alert("Log in Successfull");
-         console.log("newbalance",newBalance);
-         currentBalanceElement.innerText=newBalance;
+        setBalance(newBalance);
+         
 
        }
     else
